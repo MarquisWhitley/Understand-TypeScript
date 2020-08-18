8deb8f06fdafeeecc61a0c723a03a28437b7f1ac
Marquis Whitley <whitley.marquis1@yahoo.com>
2020-08-18 12:36:51 -0400

    basics

---
+++ app.js
@@ -0,0 +1,9 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Marquis',
+    age: 20
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,10 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+    const person = {
+    name: 'Marquis',
+    age: 20,
+}
+
+console.log(person.name)
